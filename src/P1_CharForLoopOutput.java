1b428eb4496ce95d14a63c06ee0e018567a43fc5
sniperunit234 <37313753+sniperunit234@users.noreply.github.com>
2018-04-06 21:18:05 -0400

    almost
    i didnt take into consideration it was continuing the chain

--- src/P1_CharForLoopOutput.java
+++ src/P1_CharForLoopOutput.java
@@ -1,23 +1,26 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
-*/
+	Name(s): Gabriel Lopez	
+	Date: 04/06/2018
+/* I missed calculated that it would continue adding the missing parts */
 
 /*
  Your expected results after the input is provided:
  
- 1.
- 2.
- 3. 
- 4.
+ 1.eg
+ 2.ns
+ 3. za
+ 4. qr
   
- */
+ */ 
 public class P1_CharForLoopOutput {
 
 	public static void main(String[] args) {
 		// put your call to printRange below here
-
+		printRange('e', 'g');
+		printRange('n', 's');
+		printRange('z', 'a');
+		printRange('q', 'r');
 	}
 	
 	public static void printRange(char startLetter, char endLetter ) {
